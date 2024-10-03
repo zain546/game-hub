09a07d5f4a875c9513dcd4a808da9e6583f81f7c
Zain Ali <zainali187483@gmail.com>
2024-10-03 16:44:59 +0500

    Refactoring- remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { HStack, Icon, Text } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import {
   FaWindows,
   FaPlaystation,
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,14 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
+import { Platform } from "./usePlatforms";
 
-export interface Platform{
-     id:number;
-     name:string;
-     slug: string;
-     metacritic:number;
-
-}
 export interface Game {
   id: number;
   name: string;
