5b2f7ac8362818fd96b4f39174e12c1917a06c8c
Zain Ali <zainali546official@gmail.com>
2025-01-04 22:23:32 +0500

    Build game trailer

---
+++ src/hooks/UseTrailers.ts
@@ -0,0 +1,12 @@
+import { Trailer } from './../entities/Trailer';
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+
+const useTrailers = (gameId:number) => {
+    const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+    return useQuery({
+        queryKey: ['trailers', gameId],
+        queryFn: apiClient.getAll
+    });
+}
+export default useTrailers;
